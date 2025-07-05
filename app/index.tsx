ac76089555a9e734a2a3d5837e07dc8e19bb1be4
민경호 <1736s@naver.com>
2025-07-05 18:40:05 +0900

    feat: 홈 플랫 리스트 (offer) 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,64 @@
-import { Text, View } from "react-native";
+import { images, offers } from "@/constants";
+import cn from "clsx";
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Fast Food!
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const { id, title, image, color } = item;
+          const isEven = index % 2 === 0;
+
+          return (
+            <View key={id}>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
+                style={{ backgroundColor: color }}
+                android_ripple={{ color: "#FFFF22" }}
+              >
+                {({ pressed }) => {
+                  return (
+                    <Fragment>
+                      <View className="h-full w-1/2">
+                        <Image
+                          source={image}
+                          className="size-full"
+                          resizeMode="contain"
+                        />
+                      </View>
+                      <View
+                        className={cn(
+                          "offer-card__info",
+                          isEven ? "pl-10" : "pr-10"
+                        )}
+                      >
+                        <Text className="h1-bold text-white leading-tight">
+                          {title}
+                        </Text>
+                        <Image
+                          source={images.arrowRight}
+                          className="size-10"
+                          resizeMode="contain"
+                          tintColor="#FFFFFF"
+                        />
+                      </View>
+                    </Fragment>
+                  );
+                }}
+              </Pressable>
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerClassName="pb-28 px-5"
+      />
+    </SafeAreaView>
   );
 }
