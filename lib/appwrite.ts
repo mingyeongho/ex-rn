c4a4dd054fe046a41323af15721ce7008492b9cd
민경호 <1736s@naver.com>
2025-07-07 18:54:05 +0900

    chore: create User collection in Appwrite

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID,
 };
