70680afd871dfc5a4644b45b253577c3211003f0
민경호 <1736s@naver.com>
2025-07-07 18:43:39 +0900

    chore: setup appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
+};
